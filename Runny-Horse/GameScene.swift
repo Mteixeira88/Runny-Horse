37867b0f9067a80de4b11b36754bc4e7c1751655
Miguel Teixeira <mteixeira88013@gmail.com>
2020-02-19 15:53:15 +0000

    feat: added first layout and animations

--- Runny-Horse/GameScene.swift
+++ Runny-Horse/GameScene.swift
@@ -11,63 +11,60 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var jumpHorse = SKSpriteNode()
+    var runHorse = SKSpriteNode()
+    
+    var jumpAtlas = SKTextureAtlas()
+    var jumpTextureArray = [SKTexture]()
+    
+    var runAtlas = SKTextureAtlas()
+    var runTextureArray = [SKTexture]()
+    
+    var horseIsRunning = false
     
     override func didMove(to view: SKView) {
+        physicsWorld.gravity = CGVector(dx:0, dy:-2)
+        runAtlas = SKTextureAtlas(named: "Run")
+        jumpAtlas = SKTextureAtlas(named: "Jump")
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        runHorse = SKSpriteNode(imageNamed: runAtlas.textureNames[0])
+        runHorse.size = CGSize(width: 100, height: 100)
+        runHorse.position.x = frame.minX + 75
+        runHorse.physicsBody = SKPhysicsBody(texture: runHorse.texture!, size: runHorse.texture!.size())
+        
+        addChild(runHorse)
+        
+        for i in 1...jumpAtlas.textureNames.count {
+            let name = "jump_\(i).png"
+            jumpTextureArray.append(SKTexture(imageNamed: name))
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+        for i in 1...runAtlas.textureNames.count {
+            let name = "run_\(i).png"
+            runTextureArray.append(SKTexture(imageNamed: name))
         }
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+       
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+       
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        guard horseIsRunning else {
+            horseIsRunning = true
+            runHorse.run(SKAction.repeatForever(SKAction.animate(with: runTextureArray, timePerFrame: 0.1)))
+            return
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        runHorse.run(SKAction.repeat(SKAction.animate(with: jumpTextureArray, timePerFrame: 0.1), count: 1))
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
